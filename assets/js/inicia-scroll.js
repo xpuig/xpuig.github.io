7adf608e1dcb57a40899cf024ce9e5da4ed5a49c
Xavi <xavipuig@gmail.com>
2020-07-31 19:24:18 +0200

    Fixing layout

--- assets/js/inicia-scroll.js
+++ assets/js/inicia-scroll.js
@@ -1,13 +1,48 @@
+var logo = basicScroll.create({
+  elem: document.querySelector('#logo'),
+  from: '0',
+  to: '200',
+  props: {
+    '--opacidad-logo': {
+      from: 1,
+      to: 0
+    }
+  }
+});
+
 var cabecera = basicScroll.create({
     elem: document.querySelector('#work-experience'),
-    from: '550',
-    to: '950',
+    from: '1050',
+    to: '1350',
     props: {
       '--opacidad-nav': {
         from: 0,
         to: 1
+      },
+      '--translateY': {
+        from: `${ 100 }px`,
+        to: 0
       }
     }
 });
 
+var showingGallery = basicScroll.create({
+  elem: document.querySelector('#intro-gallery'),
+  from: '300',
+  to: '500',
+  props: {
+    '--opacidad-gal': {
+      from: 0,
+      to: 1
+    },
+    '--translateY': {
+      from: `${ 100 }px`,
+      to: 0
+    }
+    
+  }
+});
+
 cabecera.start();
+showingGallery.start();
+logo.start();
